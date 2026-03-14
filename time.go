ea6f8f6271a346cc3109d43e82d592d69efcb913
Mark Solante <72701348+denzeysenpai@users.noreply.github.com>
2026-03-14 13:48:16 +0800

    [1.4.3: Delay (New)]

---
+++ time.go
@@ -0,0 +1,12 @@
+package miru
+
+import (
+	"time"
+)
+
+// Sleep pauses execution for the given number of milliseconds.
+// Example: debug.Sleep(500) // sleeps for 500ms
+func Delay(ms int) {
+	d := time.Duration(ms) * time.Millisecond
+	time.Sleep(d)
+}
