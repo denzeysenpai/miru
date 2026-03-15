132b84f543aa9382bc78f21635c7f960e6e390f0
Mark Solante <72701348+denzeysenpai@users.noreply.github.com>
2026-03-15 17:18:00 +0800

    [1.8.11: DIsplay Out | Less tabs (Enhancement)]

--- colors.go
+++ colors.go
@@ -3,10 +3,10 @@ package miru
 import "fmt"
 
 const (
-	ansiReset   = "\033[0m"
-	ansiRed     = "\033[31m"
-	ansiGreen   = "\033[32m"
-	ansiYellow  = "\033[33m"
+	ansiReset  = "\033[0m"
+	ansiRed    = "\033[31m"
+	ansiGreen  = "\033[32m"
+	ansiYellow = "\033[33m"
 )
 
 func (d *Debugger) red(s string) string {
@@ -34,13 +34,13 @@ func (d *Debugger) formatCatchLine(dateTime, location, caught string) string {
 	tag := d.red("[Miru Catch]")
 	dt := d.yellow(dateTime)
 	msg := d.red(caught)
-	return fmt.Sprintf("%s:\t%s\t%s\t->\t%s", tag, dt, location, msg)
+	return fmt.Sprintf("%s: %s\t%s\t->\t%s", tag, dt, location, msg)
 }
 
 func (d *Debugger) formatOutLine(dateTime, location, value string) string {
 	tag := d.red("[Miru Out]")
 	dt := d.yellow(dateTime)
-	return fmt.Sprintf("%s:\t%s\t%s\t->\t%s", tag, dt, location, value)
+	return fmt.Sprintf("%s: %s\t%s\t->\t%s", tag, dt, location, value)
 }
 
 func (d *Debugger) formatTestLine(dateTime, funcName, status, duration string, passed bool) string {
@@ -53,24 +53,24 @@ func (d *Debugger) formatTestLine(dateTime, funcName, status, duration string, p
 	} else {
 		statusColored = d.red(status)
 	}
-	return fmt.Sprintf("%s:\t%s\t%s\t->\t%s\t%s", tag, dt, funcName, statusColored, ms)
+	return fmt.Sprintf("%s: %s\t%s\t->\t%s\t%s", tag, dt, funcName, statusColored, ms)
 }
 
 func (d *Debugger) formatTraceLine(dateTime, name, duration string) string {
 	tag := d.green("[Miru Trace]")
 	dt := d.yellow(dateTime)
 	ms := d.yellow(duration)
-	return fmt.Sprintf("%s:\t%s\t%s\t->\t%s", tag, dt, name, ms)
+	return fmt.Sprintf("%s: %s\t%s\t->\t%s", tag, dt, name, ms)
 }
 
 func (d *Debugger) formatWalkHeader(dateTime, location, typeSummary string) string {
 	tag := d.green("[Miru Walk]")
 	dt := d.yellow(dateTime)
-	return fmt.Sprintf("%s:\t%s\t%s\t->\t%s", tag, dt, location, typeSummary)
+	return fmt.Sprintf("%s: %s\t%s\t->\t%s", tag, dt, location, typeSummary)
 }
 
 func (d *Debugger) formatCheckStackHeader(dateTime, location string) string {
 	tag := d.green("[Miru CheckStack]")
 	dt := d.yellow(dateTime)
-	return fmt.Sprintf("%s:\t%s\t%s\t->\tgoroutine stack", tag, dt, location)
+	return fmt.Sprintf("%s: %s\t%s\t->\tgoroutine stack", tag, dt, location)
 }
